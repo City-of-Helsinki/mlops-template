23a6fe2b7186548d1f533d81e2571e3737e26015
Nuutti Kytö <nuutti.kyto@hel.fi>
2022-12-05 12:45:18 +0000

    requirements under their own folder, now .devcontainer, compose and dockerfiles add requirements to image from there

--- requirements/update_requirements.sh
+++ requirements/update_requirements.sh
@@ -4,10 +4,10 @@
 
 # DEV-REQUIREMENTS for developing & updating models
 ## update dev-requirements.txt
-pip-compile --upgrade --generate-hashes --allow-unsafe -o dev-requirements.txt base-requirements.in ml-requirements.in dev-requirements.in 
+pip-compile --upgrade --generate-hashes --allow-unsafe --resolver=backtracking -o dev-requirements.txt requirements.in dev-requirements.in 
 
 # API_REQUIREMENTS for running the model & api in production
-pip-compile --upgrade --generate-hashes --allow-unsafe -o api-requirements.txt base-requirements.in ml-requirements.in api-requirements.in
+pip-compile --upgrade --generate-hashes --allow-unsafe --resolver=backtracking -o api-requirements.txt requirements.in api-requirements.in
 
 # to install requirements, run:
 # pip install -r dev-requirements.txt
