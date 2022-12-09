22a291e54e04392ff1fad9aa93f357b603384ae4
Nuutti Kytö <nuutti.kyto@hel.fi>
2022-12-09 09:17:08 +0000

    clean entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,13 +8,7 @@ then
 elif [[ $MODE = dev ]]
 then
     tail -f /dev/null
-    cd dev
-elif [[ $MODE = devapi ]]
-then
-    cd api
-    uvicorn main:app --reload --reload-include *.pickle --host 0.0.0.0
-    cd ..
-
+    
 else
-    echo "unknown mode: "$MODE", use 'api', 'dev' or 'apidev' or leave empty (defaults to 'dev')"
+    echo "unknown mode: "$MODE", use 'api', 'dev' or leave empty (defaults to 'dev')"
 fi
