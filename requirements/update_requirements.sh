20a71ac3a88e9fa37b62725df34bc82b971623bc
Nuutti Kytö <nuutti.kyto@hel.fi>
2022-12-12 13:25:53 +0000

    clean unused .txt requirements & update docs on update_requirements.sh

--- requirements/update_requirements.sh
+++ requirements/update_requirements.sh
@@ -2,9 +2,6 @@
 ## script to update requirements.txt
 ## prior running, manually write desired updates .in files
 
-# DEV-REQUIREMENTS for developing & updating models
-## update dev-requirements.txt
-# TODO: cleanup
 pip-compile --upgrade --generate-hashes --allow-unsafe --resolver=backtracking -o requirements.txt requirements.in dev-requirements.in api-requirements.in model_store-requirements.in
 
 # to install requirements, run:
