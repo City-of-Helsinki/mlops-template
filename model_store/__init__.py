d4db2dbff3486ae54188a07b2082d932fdfb59cd
Nuutti Kytö <nuutti.kyto@hel.fi>
2022-12-20 14:06:51 +0000

    add ModelSchemaContainer to model_store base, clean model_store_pickle examle notebook

--- model_store/__init__.py
+++ model_store/__init__.py
@@ -1,3 +1,3 @@
 from .model_store import ModelStore
-from .pickle_model_store import PickleModelStore
+from .pickle_model_store import PickleModelStore, ModelSchemaContainer
 from .mlflow_model_store import MlFlowModelStore
